875bf4f381fb98feaa3bdbc25fb5e17658cf8e5e
yuanyaru <398916756@qq.com>
2019-07-16 10:12:24 +0800

    requests带参数的请求与github api 互动

--- requests/main.py
+++ requests/main.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
-# -*-coding:utf-8 -*-
+# -*- coding:utf-8 -*-
+"""
+带参数的请求：
+1. URL Parameters:
+    params: request.get(url, params={'key1':'values1'})
+2. 表单参数提交
+    requests.post(url, data={'key1':'value1','key2':'value2'})
+3. json参数提交
+    requests.post(url, json={'key1':'value1','key2':'value2'})
+"""
 
 import json
 import requests
@@ -18,8 +27,26 @@ def better_print(json_str):
 def request_method():
     # response = requests.get(build_uri('users/yuanyaru'))
     response = requests.get(build_uri('user/emails'), auth=('yuanyaru', '930313yyr'))
-    print(better_print(response.text))    
+    print(better_print(response.text))
+
+
+def params_request():
+    response = requests.get(build_uri('users'), params={'since': '11'})
+    print better_print(response.text)
+    print response.request.headers
+    print response.url
+
+
+def json_request():
+    # response = requests.patch(build_uri('user'), auth=('yuanyaru', '930313yyr'), json={'name': 'GM-yyr'})
+    response = requests.post(build_uri('user/emails'), auth=('yuanyaru', '930313yyr'), json=['helloworld@github.com'])
+    print better_print(response.text)
+    print response.request.headers
+    print response.request.body
+    print response.status_code
 
 
 if __name__ == '__main__':
-    request_method()
+    # request_method()
+    # params_request()
+    json_request()
