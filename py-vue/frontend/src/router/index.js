6b2d3ef576f1f15f75029a6e03cbb157b5020d62
yuanyaru <398916756@qq.com>
2019-09-30 16:09:05 +0800

    update

--- py-vue/frontend/src/router/index.js
+++ py-vue/frontend/src/router/index.js
@@ -2,22 +2,28 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import First from '@/components/First'
+import container from '@/components/container'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
+	routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+		path: '/',
+		name: 'HelloWorld',
+		component: HelloWorld
     },
-		{
-				path: '/first',
-				name: 'First',
-				component: First,
-		},
-  ],
-	// 让 URL 变成http://localhost：8080/ping的形式。如果，不加该设置，默认的 URL 为http://localhost:8080/#/ping的形式。
+	{
+		path: '/first',
+		name: 'First',
+		component: First
+	},
+	{
+		path: '/container',
+		name: 'container',
+		component: container
+	},		
+    ],
+	// 让 URL 变成http://localhost:8080/ping的形式。如果，不加该设置，默认的 URL 为http://localhost:8080/#/ping的形式。
 	mode: 'history',
 });
